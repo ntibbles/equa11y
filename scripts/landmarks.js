d0e74026c63a1c88b7129ff540a1cfea62699a43
Noel Tibbles <ntibbles@gmail.com>
2024-11-14 06:38:13 -0500

    Fixed landmarks to only show type

--- scripts/landmarks.js
+++ scripts/landmarks.js
@@ -10,18 +10,22 @@ export function toggleLandmarkOutlines(isChecked) {
                 element.style.border = '2px solid blue';
                 element.style.position = 'relative';
                 ariaLabel = element.getAttribute('aria-label');
+                if(landmark.indexOf("role") > -1) {
+                    landmark = landmark.substring(7, landmark.indexOf(']') - 1);
+                }
 
                 // Check if label already exists
                 if (!element.querySelector('.landmark-label')) {
                     const label = document.createElement('div');
                     label.textContent = ariaLabel ? `${ariaLabel} (${landmark})` : landmark;
                     label.classList.add('landmark-label');
+                    label.style.textTransform = 'capitalize';
                     label.style.position = 'absolute';
                     label.style.top = '0';
                     label.style.left = '0';
                     label.style.backgroundColor = 'blue';
                     label.style.padding = '2px';
-                    label.style.fontSize = '12px';
+                    label.style.fontSize = '1em !important';
                     label.style.color = 'white';
                     label.style.border = '1px solid blue';
                     label.style.zIndex = '10000'; // Ensures label stays on top
