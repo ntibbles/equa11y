3e66b25d277ce8ea4527a7bb61961eb1c90c0755
Noel Tibbles <ntibbles@gmail.com>
2024-11-15 14:20:26 -0500

    Change viewport to fixed position

--- scripts/viewport.js
+++ scripts/viewport.js
@@ -2,16 +2,15 @@ export function revealViewportTag(isChecked) {
     const viewportTag = document.querySelector('meta[name="viewport"]');
     const viewportTextElement = document.createElement('div');
     if (isChecked) {
-        const viewportText = viewportTag.outerHTML;
-        viewportTextElement.innerText = viewportText;
-        viewportTextElement.id = 'vp'
-        viewportTextElement.style.position = 'fixed';
-        viewportTextElement.style.zIndex = 10000;
-        viewportTextElement.style.backgroundColor = 'blue';
-        viewportTextElement.style.color = 'white';
-        viewportTextElement.style.border = '2px solid blue';
-        document.body.prepend(viewportTextElement);
+        if(!document.getElementById('at3-label')) {
+            const viewportText = viewportTag.outerHTML;
+            viewportTextElement.innerText = viewportText;
+            viewportTextElement.className = 'at3-label';
+            viewportTextElement.style.position = 'fixed';
+            viewportTextElement.id = 'at3-label';
+            document.body.prepend(viewportTextElement);
+        }
     } else {
-        document.getElementById('vp')?.remove();
+        document.getElementById('at3-label')?.remove();
     }
 }
