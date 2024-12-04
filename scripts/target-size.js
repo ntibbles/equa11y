e90aa359d49458b79a586b7614d394a010d0df5c
Noel Tibbles <ntibbles@gmail.com>
2024-12-04 07:10:18 -0500

    New utility to teste target size

--- scripts/target-size.js
+++ scripts/target-size.js
@@ -1,32 +1,45 @@
 export function toggleTargetSize(isChecked) {
     const interactiveRoles = ['button', 'a', 'checkbox', 'radio', 'slider', 'textbox', 'combobox', 'menuitem', 'option', '[role="button"]', '[role="link"]'];
-    const clsList = ['equa11y-label', 'equa11y-size'];
+    const roleList =  document.querySelectorAll(interactiveRoles.join(','));
+    const clsList = ['equa11y-label'];
+    const elList = ['equa11y-border', 'equa11y-size'];
 
     isChecked ?  targetSize_checked() : targetSize_unchecked();
 
     function targetSize_checked() {
-        document.querySelectorAll(interactiveRoles.join(',')).forEach(element => {
-            element.style.border = '2px solid blue';
-            element.style.position = 'relative';
+       roleList.forEach(element => {
+            if(!element.classList.contains('equa11y-size')) {
+                const dimensions = getComputedDimensions(element);
+                const label = document.createElement('div');
+                label.innerHTML = dimensions.width + 'px w x \n' + dimensions.height + 'px h';
+                label.classList.add(...clsList);
 
-            const dimensions = getComputedDimensions(element);
-            const label = document.createElement('div');
-            label.innerHTML = `${dimensions.width} w x ${dimensions.height} h`;
-            label.classList.add(...clsList);
+                if(dimensions.width < 24 || dimensions.height < 24) {
+                    label.style.cssText = 'background-color: darkred !important;  outline: 2px dashed black;';
+                }
 
-            element.prepend(label);
+                element.classList.add(...elList);
+                element.parentNode.insertBefore(label, element);
+            }
         });
     }
 
     function targetSize_unchecked() {
+        document.querySelectorAll('.equa11y-size').forEach(el => {
+            el.classList.remove(...elList);
+        });
 
+        document.querySelectorAll('.equa11y-label').forEach(el => {
+            el.remove();
+        });
     }
 
     function getComputedDimensions(element) {
-        const computedStyle = getComputedStyle(element);
+       
+        console.log('width: ', element.offsetWidth);
         const dimensions = {
-            width: computedStyle.width,
-            height: computedStyle.height
+            width: element.offsetWidth,
+            height:element.offsetHeight
         }
         
         return dimensions;
