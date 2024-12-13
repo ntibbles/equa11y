b1e0f546b88572bb27cec9f6491d7d4987f6c69c
Noel Tibbles <ntibbles@gmail.com>
2024-12-13 06:14:31 -0500

    Mimic other browsers zoom text

--- scripts/text-zoom.js
+++ scripts/text-zoom.js
@@ -3,9 +3,14 @@ export function toggleZoom(isChecked) {
     const textElements = document.querySelectorAll("body *:not(style, script, noscript, iframe, link, embed, hr, br, img, video, canvas, footer)");
     const body = document.body;
     const prerender = [];
+    const initialFontSize = getComputedInt(body, 'fontSize');
+    const css = `* { font-size: ${initialFontSize * zoomLevel}px !important; }`;
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const style = document.createElement('style');
+    style.id = 'text_zoom_css';
     const zoomLabel = document.createElement('div');
     zoomLabel.style.position = 'fixed';
-    zoomLabel.innerHTML = 'Text zoomed 200%';
+    zoomLabel.innerHTML = `Text zoomed ${zoomLevel}%`;
     zoomLabel.className = 'equa11y-label';
     zoomLabel.id = 'equa11y_zoom';
     
@@ -20,6 +25,7 @@ export function toggleZoom(isChecked) {
 
     function textZoom_unchecked() {
         document.getElementById('equa11y_zoom').remove();
+        document.getElementById('text_zoom_css').remove();
         document.querySelectorAll('.equa11y-zoom-text').forEach(el => {
             el.classList.remove('equa11y-text-zoom');
             el.style['font-size'] = null;
@@ -32,7 +38,6 @@ export function toggleZoom(isChecked) {
         textElements.forEach((element) => {
             let attr = {
                 element,
-                lineHeight: getComputedInt(element, 'lineHeight'),
                 fontSize: getComputedInt(element, 'fontSize')
             };
             prerender.push(attr);
@@ -43,12 +48,14 @@ export function toggleZoom(isChecked) {
 
     function setFontSize() {
         prerender.forEach(el => {
-            let { element, lineHeight, fontSize } = el;
+            let { element, fontSize } = el;
             element.classList.add('equa11y-zoom-text');
             element.style['transition'] = 'font 0s';
             addStyle(element, 'font-size', fontSize * zoomLevel);
-            if(lineHeight > 1) addStyle(element, 'line-height', lineHeight);
         });
+
+        style.appendChild(document.createTextNode(css));
+        head.appendChild(style);
     }
 
     function getComputedInt(element, attr) {
