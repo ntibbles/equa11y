3163365be16f4e24efa59fbd06e3352aa4be361c
Noel Tibbles <ntibbles@gmail.com>
2025-07-16 10:04:17 -0400

    Added node name to tabbing order

--- scripts/tabbing-order.js
+++ scripts/tabbing-order.js
@@ -22,7 +22,7 @@ export function toggleTabbingOrder(isChecked) {
             ) {
                 const label = document.createElement('div');
                 let tabIndex = getTabIndex(element);
-                label.textContent = `${tabIndex}`;
+                label.textContent = `${element.nodeName.toLowerCase()} : ${tabIndex}`;
                 label.classList.add(...clsList);
 
                  // Set the label's style based on the tabIndex
